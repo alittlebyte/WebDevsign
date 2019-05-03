802ea951d7dd29b36a9dc64220170ec46976e1ec
alittlebyte <vladislavnikora@gmail.com>
2019-05-03 15:09:06 +0300

    Base for the website

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.pug',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+	rules: [
+	  {
+		test: /\.pug$/,
+		use: ['html-loader?attrs=false', 'pug-html-loader']
+      },
+	  {
+        test: /\.scss$/,
+        use: [
+            { loader: "style-loader"}, 
+            { loader: "css-loader" },
+            { loader: 'postcss-loader',
+                options: {
+                    plugins: () => [require('autoprefixer')]
+                }
+            },
+            { loader: "sass-loader" }
+        ]
+	  },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+		  options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        exclude: /node_modules/,
+        use:{
+          loader: "file-loader?name=/src/icons/[name].[ext]",
+        }
+      },
+      {
+        test: /\.(woff|ttf|svg)$/,
+        exclude: [/images/],
+        use:{
+          loader:'file-loader',
+        }
+      }
+	]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+	  filename: 'index.html',
+	  template:'src/index.pug'
+	}),
+	new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
+  ]
+}
